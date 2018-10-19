83c1409041639829ef9c6de10ca9a76bbb785d91
Shubham Sinha <shubham@usedipper.com>
2018-10-19 10:32:18 +0530

    working app

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,6 +2,8 @@ import { Navigation } from "react-native-navigation";
 import FirstTabScreen from "./FirstTabScreen";
 import SecondTabScreen from "./SecondTabScreen";
 import PushedScreen from "./PushedScreen";
+import TobTabScreen1 from "./TobTabScreen1";
+import TobTabScreen2 from "./TobTabScreen2";
 
 // register all screens of the app (including internal ones)
 export function registerScreens(store?: any, Provider?: any) {
@@ -9,6 +11,14 @@ export function registerScreens(store?: any, Provider?: any) {
   Navigation.registerComponent(
     "example.SecondTabScreen",
     () => SecondTabScreen
+  );
+  Navigation.registerComponent(
+    "example.FirstTabScreen.tab1",
+    () => TobTabScreen1
+  );
+  Navigation.registerComponent(
+    "example.FirstTabScreen.tab2",
+    () => TobTabScreen2
   );
   Navigation.registerComponent("example.PushedScreen", () => PushedScreen);
 }
